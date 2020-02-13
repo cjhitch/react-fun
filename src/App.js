4fbf226136c7efea89057479680100075e283389
Christopher Hitchcock <Christopher.Hitchcock@creditone.com>
2020-02-12 16:03:42 -0800

    updated to functional, going back to class based for remainder of tutorial

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import Person from './Person/Person';
 
-function App() {
+const App = props => {
+	const [ personsState, setPersonsState ] = useState( {
+		persons: [
+			{ name: 'Max', age: 28 },
+			{ name: 'Manu', age: 29 },
+			{ name: 'Stephanie', age: 26 }
+		]
+	});
+
+	const [otherState, setOtherState] = useState({
+		othersState: 'some other value'
+	});
+
+	console.log(personsState, otherState);
+
+	const switchNameHandler = () => {
+		setPersonsState({
+			persons: [
+				{ name: 'Maximilian', age: 28 },
+				{ name: 'Manu', age: 29 },
+				{ name: 'Stephanie', age: 27 }
+			]
+		});
+	}
+
 	return (
 		<div className="App">
 			<h1>Hi, I'm a React App</h1>
 			<p>This is really working!</p>
-			<Person name="Max" age="28"/>
-			<Person name="Manu" age="29">My Hobbies: Racing</Person>
-			<Person name="Stephanie" age="26"/>
+			<button onClick={switchNameHandler}>Switch Name</button>
+			<Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+			<Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
+			<Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
 		</div>
 	);
 }
